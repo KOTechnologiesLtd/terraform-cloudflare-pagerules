6cabd25288c946788543f8b42ad29e150f6dbdf0
shrestharajatko <rajat.shrestha@kotechnologies.co.uk>
2022-12-09 15:02:43 +0000

    Add tests

--- tests/defaults/defaults.tf
+++ tests/defaults/defaults.tf
@@ -9,6 +9,49 @@ terraform {
   }
 }
 
+variable zone_id {
+  value = string
+  default = "788dsagy78qew8ds123r"
+}
+
+variable "pagerules" {
+  value = map
+  pagerules = {
+    "001" = {
+      priority = 1,
+      status   = "active",
+      target   = "https://www.example.com/page1/heading*",
+      actions = {
+        resolve_override = {
+        value = "sites.example.com" },
+        host_header_override = {
+        value = "sites.example.digital" },
+      }
+    },
+    "002" = {
+      priority = 2,
+      status   = "active",
+      target   = "https://example.com/*",
+      actions = {
+        forwarding_url = {
+          url = "https://www.example.com/$1",
+        status_code = "301" }
+      }
+    },
+    "003" = {
+      priority = 3,
+      status   = "active",
+      target   = "https://www.example.com/page2*",
+      actions = {
+        resolve_override = {
+        value = "this.example.com" },
+        host_header_override = {
+        value = "this.example.com" },
+      }
+    },
+  }
+}
+
 module "main" {  
     source = "../.."   
 }
